02be1d1eb1b1f4f9357a296f70579de197d7713d
ebd1004alu <estelavictoriaballester@hotmail.es>
2025-11-16 13:32:32 +0100

    Define domain entities
    - Created classes for domain entities.
    - Added basic properties and relationships between entities.
    - Included constructors.
    
    Closes #13

--- src/CleanGo.Domain/Entities/Booking.cs
+++ src/CleanGo.Domain/Entities/Booking.cs
@@ -1,15 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿
 namespace CleanGo.Domain.Entities
 {
     public class Booking
     {
-        public int Id { get; set; }
-        public int UserId { get; set; }
+        public Guid Id { get; set; }
+        public Guid UserId { get; set; }
+        public Guid CleanerId { get; set; }
+        public Guid ServiceId { get; set; }
         public DateTime BookingDate { get; set; }
+        public string Status { get; set; } = null!;
+
+        private Booking() { } // EF Core
+
+        public Booking(Guid userId, Guid cleanerId, Guid serviceId, DateTime bookingDate, string status)
+        {
+            Id = Guid.NewGuid();
+            UserId = userId;
+            CleanerId = cleanerId;
+            ServiceId = serviceId;
+            BookingDate = bookingDate;
+            Status = status;
+        }
     }
 }
---
+++ src/CleanGo.Domain/Entities/Service.cs
@@ -0,0 +1,22 @@
+﻿namespace CleanGo.Domain.Entities
+{
+    public class Service
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; } = null!;
+        public string Description { get; set; } = null!;
+        public decimal Price { get; set; }
+        public TimeSpan Duration { get; set; }
+
+        private Service() { } // EF Core
+
+        public Service(string name, string description, decimal price, TimeSpan duration)
+        {
+            Id = Guid.NewGuid();
+            Name = name;
+            Description = description;
+            Price = price;
+            Duration = duration;
+        }
+    }
+}
