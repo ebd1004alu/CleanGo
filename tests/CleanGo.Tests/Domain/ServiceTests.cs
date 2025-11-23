3faecd465c2b2c14dcea887b8d7fef569d605060
ebd1004alu <estelavictoriaballester@hotmail.es>
2025-11-23 21:37:57 +0100

    Add Unit tests for Domain and Application
    - Written unit test for domain entitites.
    - Ensured GitHub Action runs the tests successfully.
    
    #Closes #17

---
+++ tests/CleanGo.Tests/Domain/ServiceTests.cs
@@ -0,0 +1,23 @@
+﻿using CleanGo.Domain.Entities;
+
+namespace CleanGo.Tests.Domain
+{
+    public class ServiceTests
+    {
+        [Fact]
+        public void Constructor_Should_Set_Properties_Correctly()
+        {
+            var service = new Service
+                (
+                "Service 1",
+                "Service description",
+                10,
+                new TimeSpan(3, 2, 12)
+                );
+            Assert.Equal("Service 1", service.Name);
+            Assert.Equal("Service description", service.Description);
+            Assert.Equal(10, service.Price);
+            Assert.Equal(new TimeSpan(3, 2, 12), service.Duration);
+        }
+    }
+}
---
+++ tests/CleanGo.Tests/Domain/UserTests.cs
@@ -0,0 +1,31 @@
+﻿using CleanGo.Domain.Entities;
+
+namespace CleanGo.Tests.Domain
+{
+    public class UserTests
+    {
+        [Fact]
+
+        public void Constructor_Should_Set_Properties_Correctly()
+        {
+            var dateOfBirth = DateTime.UtcNow.AddYears(-25);
+            var createdAt = DateTime.UtcNow;
+            var user = new User
+                (
+                "Test",
+                "User Domain",
+                "123456789",
+                "testuser@example.com",
+                "Address 1",
+                "hashedPassword",
+                dateOfBirth,
+                createdAt
+                );
+
+            Assert.Equal("Test", user.FirstName);
+            Assert.Equal("User Domain", user.LastName);            
+            Assert.Equal(dateOfBirth, user.DateOfBirth);
+            Assert.Equal(createdAt, user.CreatedAt);    
+        }
+    }
+}
